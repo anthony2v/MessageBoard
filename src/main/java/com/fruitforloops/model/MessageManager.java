570c2f584a09678943951947c1c293109db01158
anthony2v <anthony_2v@hotmail.com>
2020-11-12 19:10:59 -0500

    Implemented createMessage

--- src/main/java/com/fruitforloops/model/MessageManager.java
+++ src/main/java/com/fruitforloops/model/MessageManager.java
@@ -3,9 +3,11 @@ package com.fruitforloops.model;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Date;
+import com.fruitforloops.model.dao.*;
 
 public class MessageManager
 {
+	MessageDAO mdao = new MessageDAO();
 	public List<Message> getMessages(Date fromDate, Date toDate)
 	{
 		ArrayList<Message> messageList = new ArrayList<Message>();
@@ -18,6 +20,8 @@ public class MessageManager
 	public void createMessage(String user, String messageText)
 	{
 		// create new message
+		Message newMessage = new Message(user, messageText);
+		mdao.save(newMessage);
 	}
 }
 
