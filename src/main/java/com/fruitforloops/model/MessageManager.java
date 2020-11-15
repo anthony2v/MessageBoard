89c7169f1976b8ba9dac09ce334c7dcaace4624d
anthony2v <anthony_2v@hotmail.com>
2020-11-14 19:17:08 -0500

    Recreated changes from previous commits

--- src/main/java/com/fruitforloops/model/MessageManager.java
+++ src/main/java/com/fruitforloops/model/MessageManager.java
@@ -1,23 +1,62 @@
 package com.fruitforloops.model;
 
-import java.util.List;
 import java.util.ArrayList;
 import java.util.Date;
+import com.fruitforloops.model.dao.*;
 
 public class MessageManager
 {
-	public List<Message> getMessages(Date fromDate, Date toDate)
+	MessageDAO mdao = new MessageDAO();
+	MessageAttachmentDAO madao = new MessageAttachmentDAO();
+
+	public void createMessage(Message newMessage)
 	{
-		ArrayList<Message> messageList = new ArrayList<Message>();
-		
-		// populate messageList with most recent messages
-		
+		// create new message
+		mdao.save(newMessage);
+	}
+
+	public void deleteMessage(long messageId)
+	{
+		// delete message with corresponding ID
+		mdao.delete(messageId);
+	}
+
+	public void deleteMessageAttachment(long attachmentId)
+	{
+		// deletes specified attachment
+		madao.delete(attachmentId);
+	}
+
+	public MessageAttachment getMessageAttachment(long attachmentId)
+	{
+		// returns requested attachment
+		return madao.get(attachmentId);
+	}
+	
+	public ArrayList<Message> getMessages(Date fromDate, Date toDate, String[] authors, String[] hashtags)
+	{
+		// populate messageList with message between fromDate and toDate, and includes the given authors and hashtags
+		ArrayList<Message> messageList = (ArrayList<Message>)mdao.getAll();
+		messageList.removeIf((message) -> {
+			if (message.getCreatedDate().after(fromDate) && message.getCreatedDate().before(toDate))
+				for (String author: authors)
+					if (message.getAuthor().equals(author))
+						for (String hashtag: hashtags)
+							if (message.getHashtags().contains(hashtag))
+								return false;
+			return true;
+		});
+
 		return messageList;
 	}
 
-	public void createMessage(String user, String messageText)
+	public void updateMessage(Message message, long[] filesToDelete)
 	{
-		// create new message
+		// update the desired message
+		if (mdao.update(message))
+			// delete series of message attachments
+			for (long id: filesToDelete)
+				madao.delete(id);
 	}
 }
 
