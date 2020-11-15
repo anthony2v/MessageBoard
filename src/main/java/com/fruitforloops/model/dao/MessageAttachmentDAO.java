e91ae0a21f3a1f951d8dc4e080d7e3bddbad7278
TenaciousDan <danieldavidrinaldi@gmail.com>
2020-11-14 21:33:41 -0500

    MessageAttaachmentDAO done
    - also fixed an error in MessageController

--- src/main/java/com/fruitforloops/model/dao/MessageAttachmentDAO.java
+++ src/main/java/com/fruitforloops/model/dao/MessageAttachmentDAO.java
@@ -2,6 +2,10 @@ package com.fruitforloops.model.dao;
 
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.fruitforloops.HibernateUtil;
 import com.fruitforloops.model.MessageAttachment;
 
 public class MessageAttachmentDAO implements IDAO<MessageAttachment>
@@ -10,35 +14,53 @@ public class MessageAttachmentDAO implements IDAO<MessageAttachment>
 	@Override
 	public MessageAttachment get(long id)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		MessageAttachment attachment = null;
+		Transaction transaction = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			transaction = session.beginTransaction();
+
+			attachment = session.get(MessageAttachment.class, id);
+
+			transaction.commit();
+		} 
+		catch (Exception e) 
+		{
+			System.err.println("An error occured when trying to save a Message.\n" + e.getMessage());
+
+			if (transaction != null)
+				transaction.rollback();
+
+			return attachment;
+		}
+
+		return attachment;
 	}
 
 	@Override
 	public List<MessageAttachment> getAll()
 	{
-		// TODO Auto-generated method stub
+		//
 		return null;
 	}
 
 	@Override
 	public boolean save(MessageAttachment object)
 	{
-		// TODO Auto-generated method stub
+		//
 		return false;
 	}
 
 	@Override
 	public boolean update(MessageAttachment object)
 	{
-		// TODO Auto-generated method stub
+		//
 		return false;
 	}
 
 	@Override
 	public boolean delete(long id)
 	{
-		// TODO Auto-generated method stub
+		//
 		return false;
 	}
 
