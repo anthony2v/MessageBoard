116bfa115d729c80ae1ac52da79318694342d2a1
TenaciousDan <danieldavidrinaldi@gmail.com>
2020-11-14 23:46:25 -0500

    message attachment delete done

--- src/main/java/com/fruitforloops/model/dao/MessageAttachmentDAO.java
+++ src/main/java/com/fruitforloops/model/dao/MessageAttachmentDAO.java
@@ -60,8 +60,26 @@ public class MessageAttachmentDAO implements IDAO<MessageAttachment>
 	@Override
 	public boolean delete(long id)
 	{
-		//
-		return false;
+		Transaction transaction = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			transaction = session.beginTransaction();
+
+			session.createQuery("delete " + MessageAttachment.class.getSimpleName() + " where id = :id")
+				.setParameter("id", id).executeUpdate();
+
+			transaction.commit();
+		} 
+		catch (Exception e) 
+		{
+			System.err.println("An error occured when trying to delete a Message Attachment.\n" + e.getMessage());
+
+			if (transaction != null)
+				transaction.rollback();
+
+			return false;
+		}
+
+		return true;
 	}
 
 }
