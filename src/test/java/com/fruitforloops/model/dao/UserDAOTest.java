896fc61343fb84dff9f4561215c8e3d69300ac3c
anthony2v <anthony_2v@hotmail.com>
2020-12-06 20:47:58 -0500

    Added comments to explain each test file

--- src/test/java/com/fruitforloops/model/dao/UserDAOTest.java
+++ src/test/java/com/fruitforloops/model/dao/UserDAOTest.java
@@ -8,21 +8,25 @@ class UserDAOTest {
 
 	@Test
 	void circularDependencyTest() {
+		// Group 2 is a parent of 4, and group 4 is a parent of 2, which is a circular dependency and is not allowed
 		assertEquals(0, new UserDAO().getAll("src/test/webapp/WEB-INF/usersCircular.json").size());
 	}
 	
 	@Test
 	void goodTest() {
+		// This is the file that our application uses
 		assertEquals(8, new UserDAO().getAll("src/test/webapp/WEB-INF/users.json").size());
 	}
 	
 	@Test
 	void undefinedGroupTest() {
+		// User ID 7, Jack123, is a member of group 8, which doesn't exist
 		assertEquals(0, new UserDAO().getAll("src/test/webapp/WEB-INF/usersBadGroup.json").size());
 	}
 	
 	@Test
 	void badParentTest() {
+		// Group ID 5, comp, has a parent of group 9, which doesn't exist
 		assertEquals(0, new UserDAO().getAll("src/test/webapp/WEB-INF/usersBadParent.json").size());
 	}
 
