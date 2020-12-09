df7309d3cb0801337ea6b563f3cd37641540c081
anthony2v <anthony_2v@hotmail.com>
2020-12-08 21:38:13 -0500

    Added more test data

--- database/db_demo_items.sql
+++ database/db_demo_items.sql
@@ -9,3 +9,6 @@ insert into message(id, message_text, created_date, last_modified_date, author, 
 insert into message(id, message_text, created_date, last_modified_date, author, group_id) values(9, 'Public two', '2020-11-28 17:28:30', '2020-12-20 00:00:00', 'Jack123', -1);
 insert into message(id, message_text, created_date, last_modified_date, author, group_id) values(10, 'Public three', '2020-11-29 17:28:30', '2020-12-20 00:00:00', 'Neo', -1);
 insert into message(id, message_text, created_date, last_modified_date, author, group_id) values(11, 'Public four', '2020-11-30 17:28:30', '2020-12-20 00:00:00', 'Daniel', -1);
+insert into message(id, message_text, created_date, last_modified_date, author, group_id) values(12, 'Message for comp people', '2020-12-04 13:28:30', '2020-12-21 00:00:00', 'Jack123', 5);
+insert into message(id, message_text, created_date, last_modified_date, author, group_id) values(13, 'From admin once more', '2020-12-08 10:46:34', '2020-12-08 10:46:34', 'Anthony', 1);
+insert into message(id, message_text, created_date, last_modified_date, author, group_id) values(14, 'Welcome to concordia group!', '2020-12-09 10:46:34', '2020-12-09 10:46:34', 'John', 2);
